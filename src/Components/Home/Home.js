7ff376849ffec81b4270823f706ef7fa41a1d1f9
shakirBunna <shakir973019@gmail.com>
2023-09-15 19:43:15 +0530

    updating App

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
-// import style from "./Home.module.scss"
+import style from "./Home.module.scss"
+
 
 
 const Home = () => {
   return (
-    <div>
-      <h1>This is home from components</h1>
+    <div className={style.home}>
+      <h1>This is home from </h1>
+      <span>heey shakir this is updated home page </span>
+      <h4>Do you find this nice ??</h4>
     </div>
   )
+
 }
 
 export default Home;
